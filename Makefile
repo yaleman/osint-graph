a4330132a8efac50a8ade15765f4d4bd88e95e88
James Hodgkinson <james@terminaloutcomes.com>
2024-01-28 18:51:23 +1000

    initial commit, it builds at least

---
+++ Makefile
@@ -0,0 +1,8 @@
+.PHONY: trunk
+trunk:
+	trunk build
+
+.PHONY: serve
+serve:
+	trunk serve
+
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+#![forbid(unsafe_code)]
+#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
+#![warn(clippy::all, rust_2018_idioms)]
+
+// // When compiling natively:
+// #[cfg(not(target_arch = "wasm32"))]
+// fn main() {
+//     use eframe::egui::Visuals;
+
+//     eframe::run_native(
+//         "Egui node graph example",
+//         eframe::NativeOptions::default(),
+//         Box::new(|cc| {
+//             cc.egui_ctx.set_visuals(Visuals::dark());
+//             #[cfg(feature = "persistence")]
+//             {
+//                 Box::new(NodeGraphExample::new(cc))
+//             }
+//             #[cfg(not(feature = "persistence"))]
+//             Box::<NodeGraphExample>::default()
+//         }),
+//     )
+//     .expect("Failed to run native example");
+// }
+
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use osint_graph::NodeGraphExample;
+    // Redirect `log` message to `console.log` and friends:
+
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                web_options,
+                Box::new(|cc| Box::new(NodeGraphExample::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {}

effa026bc189d83b4f804783a6fa2d35786504a4
James Hodgkinson <james@terminaloutcomes.com>
2024-06-21 09:12:58 +1000

    sigma.js demo from https://github.com/jacomyal/sigma.js/tree/main/packages/demo/public shoved into my code

--- osint-graph-frontend/src/App.tsx
+++ osint-graph-frontend/src/App.tsx
@@ -1,216 +1,232 @@
+import { useEffect, useMemo, useState } from "react";
+import { DirectedGraph } from "graphology";
 import {
-	useCallback,
-	useEffect,
-	useState,
+	FullScreenControl,
+	SigmaContainer,
+	ZoomControl,
+} from "@react-sigma/core";
+import { createNodeImageProgram } from "@sigma/node-image";
 
-
-} from "react";
-
-import type { MouseEvent as ReactMouseEvent } from "react";
-
+import { BiBookContent, BiRadioCircleMarked } from "react-icons/bi";
 import {
-	Background,
-	Controls,
-	MiniMap,
-	ReactFlow,
-	Panel,
-	addEdge,
-	type OnConnect,
-	useNodesState,
-	useEdgesState,
-} from "reactflow";
+	BsArrowsFullscreen,
+	BsFullscreenExit,
+	BsZoomIn,
+	BsZoomOut,
+} from "react-icons/bs";
+import { GrClose } from "react-icons/gr";
 
-import "reactflow/dist/style.css";
+import { omit } from "lodash";
+import type { Settings } from "sigma/settings";
 
-import { type CustomNode, initialNodes, nodeTypes } from "./nodes";
-import { type CustomEdge, initialEdges, edgeTypes } from "./edges";
-import { fetchProjects, projectLis, newProject } from "./api";
-import type { Project } from "./types";
-import { newNode } from "./ui";
-import {
-	forceCenter,
-	forceCollide,
-	forceLink,
-	forceManyBody,
-	forceSimulation,
-} from "d3-force";
+// import type { MouseEvent as ReactMouseEvent } from "react";
 
-function updateProjects(
-	setProjectNodes: React.Dispatch<React.SetStateAction<Project[]>>,
-) {
-	fetchProjects().then((projects) => {
-		console.debug("projects loaded", projects);
-		setProjectNodes(projects);
-	});
-}
+// import { type CustomNode, initialNodes, nodeTypes } from "./nodes";
+// import { type CustomEdge, initialEdges, edgeTypes } from "./edges";
+// import { newNode } from "./ui";
+// import { fetchProjects } from "./api";
+import type { Dataset, FiltersState } from "./types";
+import { drawHover, drawLabel } from "./sigmademo/canvas-utils";
+import GraphSettingsController from "./sigmademo/GraphSettingsController";
+import GraphEventsController from "./sigmademo/GraphEventsController";
+import GraphDataController from "./sigmademo/GraphDataController";
+import GraphTitle from "./sigmademo/GraphTitle";
+import SearchField from "./sigmademo/SearchField";
+import DescriptionPanel from "./sigmademo/DescriptionPanel";
+import ClustersPanel from "./sigmademo/ClustersPanel";
+import TagsPanel from "./sigmademo/TagsPanel";
 
-const RADIUS = 8;
-const FORCE_RADIUS_FACTOR = 1.5;
-// const LINK_WIDTH = 3;
-// const LINK_DISTANCE = 30;
-const NODE_STRENGTH = -50;
-// const width = 800;
-// const height = 600;
-
-// const getId = (node: CustomNode) => node.id;
-
-// function d3Map<T, U>(
-// 	data: T[],
-// 	keyAccessor: (datum: T) => string,
-// 	valueAccessor: (datum: T) => U,
-// ): Map<string, U> {
-// 	return new Map(
-// 		Array.from(data, (datum) => [keyAccessor(datum), valueAccessor(datum)]),
-// 	);
+// /** pulls the project list  */
+// function updateProjects(
+// 	setProjectNodes: React.Dispatch<React.SetStateAction<Project[]>>,
+// ) {
+// 	fetchProjects().then((projects) => {
+// 		console.debug("projects loaded", projects);
+// 		setProjectNodes(projects);
+// 	});
 // }
 
-// export declare const useNodesState: <NodeData = any>(
-// 	initialItems: CustomNode[],
-// ) => [
-// 	CustomNode<NodeData, string | undefined>[],
-// 	Dispatch<SetStateAction<Node<NodeData, string | undefined>[]>>,
-// 	OnChange<NodeChange>,
-// ];
-// export declare const useEdgesState: <EdgeData = any>(
-// 	initialItems: CustomEdge[],
-// ) => [
-// 	CustomEdge<EdgeData>[],
-// 	Dispatch<SetStateAction<Edge<EdgeData>[]>>,
-// 	OnChange<EdgeChange>,
-// ];
+const baseDataSet = {
+	nodes: [
+		{
+			key: "cytoscape",
+			label: "Cytoscape",
+			tag: "Tool",
+			URL: "https://en.wikipedia.org/wiki/Cytoscape",
+			cluster: "1",
+			x: 643.82275390625,
+			y: -770.3126220703125,
+			score: 0.00006909602204225056,
+		},
+		{
+			key: "microsoft excel",
+			label: "Microsoft Excel",
+			tag: "Tool",
+			URL: "https://en.wikipedia.org/wiki/Microsoft%20Excel",
+			cluster: "1",
+			x: -857.2847900390625,
+			y: 602.7734375,
+			score: 0.0018317394731443256,
+		},
+	],
+	edges: [["cytoscape", "microsoft excel"]] as [string, string][],
+	clusters: [{ key: "1", color: "#6c3e81", clusterLabel: "Graph theory" }],
+	tags: [
+		{ key: "Chart type", image: "charttype.svg" },
+		{ key: "Company", image: "company.svg" },
+		{ key: "Concept", image: "concept.svg" },
+		{ key: "Field", image: "field.svg" },
+		{ key: "List", image: "list.svg" },
+		{ key: "Method", image: "method.svg" },
+		{ key: "Organization", image: "organization.svg" },
+		{ key: "Person", image: "person.svg" },
+		{ key: "Technology", image: "technology.svg" },
+		{ key: "Tool", image: "tool.svg" },
+		{ key: "unknown", image: "unknown.svg" },
+	],
+};
+
+// const emptyDataset: Dataset = {
+// 	nodes: [],
+// 	edges: [],
+// 	clusters: [],
+// 	tags: [],
+// };
 
 export default function App() {
-	const [projectNodes, setProjectNodes] = useState<Project[]>([]);
+	const [dataset, _setDataset] = useState<Dataset>(baseDataSet);
+	const [dataReady, setDataReady] = useState(false);
+	const [showContents, setShowContents] = useState(false);
 
-	const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-	const [edges, setEdges, onEdgesChange] = useEdgesState(
-		initialEdges.map((customEdge) => customEdge.edge),
-	);
-
-	const [_windowSize, setWindowSize] = useState({
-		width: window.innerWidth,
-		height: window.innerHeight,
+	const [filtersState, setFiltersState] = useState<FiltersState>({
+		clusters: {},
+		tags: {},
 	});
-
-
-	// we use the useEffect hook to listen to the window resize event
-	useEffect(() => {
-		window.onresize = () => {
-			setWindowSize({
-				width: window.innerWidth,
-				height: window.innerHeight,
-			});
-		};
-	}, []);
-
-	useEffect(() => {
-		// Create the D3 simulation
-		const simulation = forceSimulation(nodes as CustomNode[])
-			.force(
-				"link",
-				forceLink<CustomNode, CustomEdge>()
-					.id((node: CustomNode) => node.id)
-					.distance((edge: CustomEdge) => {
-						const source = nodes.find((n) => n.id === edge.source);
-						const target = nodes.find((n) => n.id === edge.target);
-						const distance = Math.sqrt(
-							((source?.position?.x || 0) - (target?.position?.x || 0)) ^ 2 +
-							((source?.position?.y || 0) - (target?.position?.y || 0)) ^ 2);
-						return distance;
-						// const horizDistance = (source?.position?.x || 0) - (target?.position?.x || 0);
-						// const vertDistance = (source?.position?.y || 0) - (target?.position?.y || 0);
-					})
-					.strength(-0.01),
-			)
-			.force("charge", forceManyBody().strength(NODE_STRENGTH / 10))
-			.force("collide", forceCollide(RADIUS + FORCE_RADIUS_FACTOR))
-			.force("center", forceCenter(0, 0).strength(1))
-			;
-
-		// Update the node positions after each tick
-		simulation.on("tick", () => {
-			setNodes((theseNodes) =>
-				theseNodes.map((thisNode) => {
-					const node = simulation.nodes().find((n) => n.id === thisNode.id);
-					// const node = simulation.find(el.position.x, el.position.y);
-					if (node !== undefined) {
-						if (node.x !== undefined && node.y !== undefined) {
-							thisNode.position = { x: node.x, y: node.y };
-						}
-					}
-					return thisNode;
-				}),
-			);
-		});
-		return () => {
-			simulation.stop();
-		};
-	}, [nodes, setNodes]);
-
-	// const onLoad = (reactFlowInstance: OnLoadParams) =>
-	// 		setReactFlowInstance(reactFlowInstance);
-
-	// handler for when a connection is made
-	const onConnect: OnConnect = useCallback(
-		(connection) => {
-			setEdges((edges) => addEdge(connection, edges));
-		},
-		[setEdges],
-	);
 
 	// on startup, pull the project list
 	useEffect(() => {
-		updateProjects(setProjectNodes);
+		// updateProjects(setProjectNodes);
+		setDataReady(true);
 	}, []); // Empty array means this effect runs once on component mount
 
-	return (
-		<ReactFlow
-			nodes={nodes}
-			nodeTypes={nodeTypes}
-			onNodeDragStop={(event: ReactMouseEvent, eventNode: CustomNode, eventNodes: CustomNode[]) => {
+	const [hoveredNode, setHoveredNode] = useState<string | null>(null);
+	const sigmaSettings: Partial<Settings> = useMemo(
+		() => ({
+			nodeProgramClasses: {
+				image: createNodeImageProgram({
+					size: { mode: "force", value: 256 },
+				}),
+			},
+			defaultDrawNodeLabel: drawLabel,
+			defaultDrawNodeHover: drawHover,
+			defaultNodeType: "image",
+			defaultEdgeType: "arrow",
+			labelDensity: 0.07,
+			labelGridCellSize: 60,
+			labelRenderedSizeThreshold: 15,
+			labelFont: "sans-serif",
+			zIndex: true,
+			allowInvalidContainer: true, // TODO: work out why this is being derpy
+		}),
+		[],
+	);
 
-				console.debug("onNodeDragStop", event, eventNode, eventNodes);
-				setNodes(nodes);
-			}}
-			onNodesChange={onNodesChange}
-			edges={edges}
-			edgeTypes={edgeTypes}
-			onEdgesChange={onEdgesChange}
-			onConnect={onConnect}
-			fitView
-		>
-			<Background />
-			<MiniMap />
-			<Controls />
-			<Panel position="top-right">
-				<button
-					type="button"
-					onClick={() => {
-						newNode(nodes, setNodes);
-					}}
-				>
-					New Node
-				</button>
-				<button
-					type="button"
-					onClick={() => {
-						updateProjects(setProjectNodes);
-					}}
-				>
-					Get Projects
-				</button>
-				<button
-					type="button"
-					onClick={() => {
-						newProject().then(() => {
-							updateProjects(setProjectNodes);
-						});
-					}}
-				>
-					New Project
-				</button>
-				{projectLis(projectNodes)}
-			</Panel>
-		</ReactFlow>
+	// if (!dataReady) {
+	// 	console.debug("No data!");
+	// 	return null;
+	// }
+
+	return (
+		<div id="app-root" className={showContents ? "show-contents" : ""}>
+			<SigmaContainer
+				graph={DirectedGraph}
+				settings={sigmaSettings}
+				className="react-sigma"
+			>
+				<GraphSettingsController hoveredNode={hoveredNode} />
+				<GraphEventsController setHoveredNode={setHoveredNode} />
+				<GraphDataController dataset={dataset} filters={filtersState} />
+
+				{dataReady && (
+					<>
+						<div className="controls">
+							<div className="react-sigma-control ico">
+								<button
+									type="button"
+									className="show-contents"
+									onClick={() => setShowContents(true)}
+									title="Show caption and description"
+								>
+									<BiBookContent />
+								</button>
+							</div>
+							<FullScreenControl className="ico">
+								<BsArrowsFullscreen />
+								<BsFullscreenExit />
+							</FullScreenControl>
+
+							<ZoomControl className="ico">
+								<BsZoomIn />
+								<BsZoomOut />
+								<BiRadioCircleMarked />
+							</ZoomControl>
+						</div>
+						<div className="contents">
+							<div className="ico">
+								<button
+									type="button"
+									className="ico hide-contents"
+									onClick={() => setShowContents(false)}
+									title="Show caption and description"
+								>
+									<GrClose />
+								</button>
+							</div>
+							<GraphTitle filters={filtersState} />
+							<div className="panels">
+								<SearchField filters={filtersState} />
+								<DescriptionPanel />
+								<ClustersPanel
+									clusters={dataset.clusters}
+									filters={filtersState}
+									setClusters={(clusters) =>
+										setFiltersState((filters) => ({
+											...filters,
+											clusters,
+										}))
+									}
+									toggleCluster={(cluster) => {
+										setFiltersState((filters) => ({
+											...filters,
+											clusters: filters.clusters[cluster]
+												? omit(filters.clusters, cluster)
+												: { ...filters.clusters, [cluster]: true },
+										}));
+									}}
+								/>
+								<TagsPanel
+									tags={dataset.tags}
+									filters={filtersState}
+									setTags={(tags) =>
+										setFiltersState((filters) => ({
+											...filters,
+											tags,
+										}))
+									}
+									toggleTag={(tag) => {
+										setFiltersState((filters) => ({
+											...filters,
+											tags: filters.tags[tag]
+												? omit(filters.tags, tag)
+												: { ...filters.tags, [tag]: true },
+										}));
+									}}
+								/>
+							</div>
+						</div>
+					</>
+				)}
+			</SigmaContainer>
+		</div>
 	);
 }
